7f236bf479689339a37e3c88a2c4acb0dec5b2e3
thimnaalam <alamthimna@gmail.com>
2024-01-10 13:46:03 +0200

    contactResume

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,19 +5,14 @@
                 Thimna's Portfolio &copy;
                 <span>{{ currYear }}</span>
             </p>
-
-            <div class="social-media-icons  ">       
-        <a href="https://www.linkedin.com/in/thimna-alam-117618297?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B9Rrg2YrGQ%2BKI1aZnH0m3tQ%3D%3D" target="_blank" rel="noopener noreferrer">
-          <i class="fab fa-linkedin"></i>
-        </a>
-        <a href="https://github.com/thimnaalam" target="_blank" rel="noopener noreferrer">
-          <i class="fab fa-github"></i>
-        </a>
-        
-      </div>
-        </div>
-
+            <!-- icons -->
+            <div class="text-center">
+        <a href="https://www.linkedin.com/in/thimna-alam-117618297?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B9Rrg2YrGQ%2BKI1aZnH0m3tQ%3D%3D" class="text-dark mx-2"><i class="ab fa-linkedin"></i></a>
+        <a href="https://github.com/thimnaalam" class="text-dark mx-2"><i class="fab fa-github"></i></a>
+         
     </div>
+     </div>
+       </div>
 </template>
 
 <script>
