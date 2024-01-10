ddc396b7fbf3d90b91d359ceeb3c6874753329e6
thimnaalam <alamthimna@gmail.com>
2024-01-10 16:30:20 +0200

    projects

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -2,7 +2,52 @@
     <div class="container">
 <div class="row">
     <h2 class="display-2" id="heading">Projects</h2>
+
+    <div class="row grid gap-3" id="rows">
+  <div class="p-2   g-col-6 card w-25 bg-dark ">
+    <div class="card-body">
+      <img src="../assets/netflix.png" class="card-img-top" alt="netflix">
+      <h5 class="card-title text-white ">Netflix Clone</h5>
+      <a href="#" class="btn btn-outline-dark" target="_blank" >Github</a>
+      <a href="#" class="btn btn-outline-dark" target="_blank">Netify</a>
+    </div>
+  </div>
+  <div class=" p-2 g-col-6 card w-25 bg-dark"> 
+    <div class="card-body">
+      <img src="../assets/ecom.jpg" class="card-img-top" alt="E.Commerce">
+      <h5 class="card-title text-white">E.Commerce</h5>
+      <a href="" class="btn btn-outline-dark" target="_blank">GitHub</a>
+      <a href="#" class="btn btn-outline-dark" target="_blank">Netify</a>
+    </div>
+  </div>
+  <div class=" p-2 g-col-6 card w-25 bg-dark"> 
+    <div class="card-body">
+      <img src="../assets/mobi.png" class="card-img-top" alt="MobiCare">
+      <h5 class="card-title text-white">MobiCare</h5>
+      <a href="https://github.com/thimnaalam/MobiCare_week1.git" class="btn btn-outline-dark" target="_blank">GitHub</a>
+      <a href="#" class="btn btn-outline-dark" target="_blank">Netify</a>
+    </div>
+  </div>
 </div>
+<div class="row grid gap-3 pt-4 " id="rows">
+  <div class="p-2   g-col-6 card w-25 bg-dark ">
+    <div class="card-body">
+      <img src="../assets/calc.png" class="card-img-top" alt="Calculator">
+      <h5 class="card-title text-white">Calculator</h5>
+      <a href="https://github.com/thimnaalam/projectCalculator.git" class="btn btn-outline-dark" target="_blank">Github</a>
+      <a href="#" class="btn btn-outline-dark" target="_blank">Netify</a>
+    </div>
+  </div>
+  <div class=" p-2 g-col-6 card w-25 bg-dark"> 
+    <div class="card-body">
+      <img src="../assets/keyboard.webp" class="card-img-top" alt="Keyboard">
+      <h5 class="card-title text-white">Keyboard</h5>
+      <a href="#" class="btn btn-outline-dark" target="_blank">GitHub</a>
+      <a href="#" class="btn btn-outline-dark" target="_blank">Netify</a>
+    </div>
+  </div>
+</div>
+    </div>
     </div>
 </template>
 
